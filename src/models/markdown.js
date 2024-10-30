acffdb33f27e557123b9cdd77462912bc65ed586
huunq <nqhuund@gmail.com>
2024-10-30 11:03:31 +0700

    #65

---
+++ src/models/markdown.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize'); // Nhập lớp Model từ thư viện sequelize. Lớp này là cơ sở cho tất cả các mô hình Sequelize.
+module.exports = (sequelize, DataTypes) => {
+    class Markdown extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+
+    // Phương thức init được sử dụng để khởi tạo mô hình Markdown với các thuộc tính và tùy chọn.
+    Markdown.init({
+        contentHTML: DataTypes.TEXT('long'),
+        contentMarkdown: DataTypes.TEXT('long'),
+        description: DataTypes.TEXT('long'),
+        doctorId: DataTypes.INTEGER,
+        specialtyId: DataTypes.INTEGER,
+        clinicId: DataTypes.INTEGER,
+    }, {
+        // Đối tượng sequelize được truyền vào để liên kết mô hình này với kết nối cơ sở dữ liệu.
+        sequelize,
+        //modelName: Tên của mô hình là Markdown
+        modelName: 'Markdown',
+    });
+    return Markdown; // Xuất mô hình Markdown 
+};
